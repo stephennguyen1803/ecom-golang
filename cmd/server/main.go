1196e6db688bfb6d311c617c1ff6e8de567e864e
Anh Dung Nguyen <anhdung.phc@gmail.com>
2024-07-11 22:06:11 +0700

    [section2] create routers and server to handle

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"ecom-project/internal/routers"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	r := routers.NewServer()
+
+	// Listen and serve on 8081
+	r.Run(":8081")
 }
