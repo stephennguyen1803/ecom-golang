12ee80f195e18e2f3c6e1c7887579184d8111f04
dungna_work <anhdung.phc@gmail.com>
2024-08-13 13:19:00 +0700

    [section-10] Add Redis Config And Apply the BenchTest Poolsize

---
+++ tests/benchmark/redis_pool_get_test.go
@@ -0,0 +1,49 @@
+package benchmark
+
+import (
+	"context"
+	"strconv"
+	"testing"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var ctxGet = context.Background()
+
+func initDataRedis(b *testing.B, ctx context.Context, rdb *redis.Client) {
+	rdb.FlushAll(ctx)
+	var key, value string
+	for i := 0; i < b.N; i++ {
+		key = "key" + strconv.Itoa(i)
+		value = "value" + strconv.Itoa(i)
+		rdb.Set(ctx, key, value, 0)
+	}
+}
+
+func BenchmarkPoolSizeGet1(b *testing.B) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Protocol: 3,
+		PoolSize: 1,
+	})
+	defer rdb.Close()
+	b.ResetTimer()
+	initDataRedis(b, ctxGet, rdb)
+	for i := 0; i < b.N; i++ {
+		rdb.Get(ctxGet, "key"+strconv.Itoa(i))
+	}
+}
+
+func BenchmarkPoolSizeGet15(b *testing.B) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Protocol: 3,
+		PoolSize: 15,
+	})
+	defer rdb.Close()
+	b.ResetTimer()
+	initDataRedis(b, ctxGet, rdb)
+	for i := 0; i < b.N; i++ {
+		rdb.Get(ctxGet, "key"+strconv.Itoa(i))
+	}
+}
