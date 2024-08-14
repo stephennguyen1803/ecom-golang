cc000c1cf38d50f2b097596ad87c9098c171307a
dungna_work <anhdung.phc@gmail.com>
2024-08-14 10:04:37 +0700

    [section-10] update the test, apply the parallel process

--- tests/benchmark/redis_pool_test.go
+++ tests/benchmark/redis_pool_test.go
@@ -4,10 +4,21 @@ import (
 	"context"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 )
 
 var ctxSet = context.Background()
+
+func setDataToRedis(b *testing.B, rdb *redis.Client) {
+	var key, value string
+	uuid := uuid.New().String()
+	key = "key_" + uuid
+	value = "value_" + uuid
+	if err := rdb.Set(ctxSet, key, value, 0).Err(); err != nil {
+		b.Fatal(err, "Failed to set data")
+	}
+}
 
 func BenchmarkPoolSize1(b *testing.B) {
 
@@ -19,9 +30,11 @@ func BenchmarkPoolSize1(b *testing.B) {
 	defer rdb.Close()
 	b.ResetTimer()
 	rdb.FlushAll(ctxSet)
-	for i := 0; i < b.N; i++ {
-		rdb.Set(ctxSet, "key", "value", 0)
-	}
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			setDataToRedis(b, rdb)
+		}
+	})
 }
 
 func BenchmarkPoolSize10(b *testing.B) {
@@ -33,7 +46,9 @@ func BenchmarkPoolSize10(b *testing.B) {
 	defer rdb.Close()
 	b.ResetTimer()
 	rdb.FlushAll(ctxSet)
-	for i := 0; i < b.N; i++ {
-		rdb.Set(ctxSet, "key", "value", 0)
-	}
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			setDataToRedis(b, rdb)
+		}
+	})
 }
