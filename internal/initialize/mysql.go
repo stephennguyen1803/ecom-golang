22f465ee5c1c2e051b4f8754db76ceafeffd5422
Anh Dung Nguyen <anhdung.phc@gmail.com>
2024-08-26 22:22:04 +0700

    [section-14] Add Code Auto Gen Model Struct Depend On table in mysql

--- internal/initialize/mysql.go
+++ internal/initialize/mysql.go
@@ -8,6 +8,7 @@ import (
 
 	"go.uber.org/zap"
 	"gorm.io/driver/mysql"
+	"gorm.io/gen"
 	"gorm.io/gorm"
 )
 
@@ -37,6 +38,7 @@ func InitMysql() {
 	setPool()
 	// create tables
 	migrateTables()
+	genTableDAO()
 }
 
 // open the pool
@@ -58,4 +60,24 @@ func migrateTables() {
 	if err != nil {
 		checkErrorPanic(err, "Failed to migrate tables")
 	}
+}
+
+func genTableDAO() {
+	g := gen.NewGenerator(gen.Config{
+		OutPath: "./internal/model",
+		Mode:    gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
+	})
+
+	// // gormdb, _ := gorm.Open(mysql.Open("root:@(127.0.0.1:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local"))
+	g.UseDB(global.Mdb) // reuse your gorm db
+
+	// // Generate basic type-safe DAO API for struct `model.User` following conventions
+	// g.ApplyBasic(model.User{})
+	g.GenerateModel("go_crm_user")
+
+	// // Generate Type Safe API with Dynamic SQL defined on Querier interface for `model.User` and `model.Company`
+	// g.ApplyInterface(func(Querier) {}, model.User{}, model.Company{})
+
+	// Generate the code
+	g.Execute()
 }
