9488eec98a6c79664dca0e52ec26937050dd6a8c
Anh Dung Nguyen <anhdung.phc@gmail.com>
2024-10-01 23:04:54 +0700

    [section20] Add Push OTP and Email to SendOTP Service

--- internal/initialize/rabbitMq.go
+++ internal/initialize/rabbitMq.go
@@ -2,16 +2,26 @@ package initialize
 
 import (
 	"ecom-project/global"
+	"fmt"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func InitRabbitMQ() {
-	conn, err := amqp.Dial("amqps://akpwryeb:UiSkc1jO9GXYtkjH2PjOVRGRtW9pdmSg@octopus.rmq3.cloudamqp.com/akpwryeb")
+	// RabbitMq config initialization
+	rabbitMQConfig := global.Config.RabbitMQ
+	// Connect to RabbitMQ Cloud
+	url := fmt.Sprintf("amqps://%s:%s@%s/%s", rabbitMQConfig.User, rabbitMQConfig.Password, rabbitMQConfig.Host, rabbitMQConfig.VHost)
+	//url := amqps://akpwryeb:UiSkc1jO9GXYtkjH2PjOVRGRtW9pdmSg@octopus.rmq3.cloudamqp.com/akpwryeb
+	conn, err := amqp.Dial(url)
 	if err != nil {
 		checkErrorPanic(err, "Failed to connect to RabbitMQ")
 	}
-	defer conn.Close()
 	global.RabbitMQConn = conn
 	global.Logger.Info("RabbitMQ Cloud connected successfully")
+	ch, err := conn.Channel()
+	if err != nil {
+		checkErrorPanic(err, "Failed to open a channel")
+	}
+	global.RabbitMQCh = ch
 }
